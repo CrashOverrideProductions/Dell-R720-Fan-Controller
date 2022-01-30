bfdd5f2b85e607bd0c4ad6bf29458dec5ca5436d
Justin Bland <46258474+CrashOverrideProductions@users.noreply.github.com>
2022-01-30 20:22:16 +1000

    proto
    Will not start service win 11

---
+++ DellFanController/DellFanController/FanControl.cs
@@ -0,0 +1,72 @@
+﻿// ====================================================================================================================
+// Application:      Dell R720 iDrac Fan Controller
+// Purpose:          Fan Control
+// Author:           Justin Bland
+// Date:             30/01/2022
+// Audit Date:       
+// Status            In Development
+// Revisions
+// ====================================================================================================================
+using System;
+using System.Diagnostics;
+
+namespace DellFanController
+{
+    internal class FanControl
+    {
+        internal void enableManualControl(string ipAddress, string username, string password)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            string ipmiToolLocation = AppDomain.CurrentDomain.BaseDirectory;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C cd " + ipmiToolLocation + "&& ipmitool -I lanplus -H " + ipAddress + " -U " + username + " -P " + password + " raw 0x30 0x30 0x01 0x00";
+            process.StartInfo = startInfo;
+            process.Start();
+
+            // string res = process.StandardOutput.ReadToEnd();
+
+            //Console.WriteLine(res);
+
+        }
+
+        internal void diableManualControl(string ipAddress, string username, string password)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            string ipmiToolLocation = AppDomain.CurrentDomain.BaseDirectory;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C cd " + ipmiToolLocation + "&& ipmitool -I lanplus -H " + ipAddress + " -U " + username + " -P " + password + " raw 0x30 0x30 0x01 0x01";
+            process.StartInfo = startInfo;
+            process.Start();
+
+            // string res = process.StandardOutput.ReadToEnd();
+
+            //Console.WriteLine(res);
+        }
+
+        internal void sendFanSpeedCommand(string ipAddress, string username, string password, int fanSpeed)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            string ipmiToolLocation = AppDomain.CurrentDomain.BaseDirectory;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C cd " + ipmiToolLocation + "&& ipmitool -I lanplus -H " + ipAddress + " -U " + username + " -P " + password + " raw 0x30 0x30 0x02 0xff " + "0x" + fanSpeed.ToString("x16");
+            process.StartInfo = startInfo;
+            process.Start();
+
+            //string res = process.StandardOutput.ReadToEnd();
+
+            //Console.WriteLine(res);
+        }
+    }
+}
---
+++ DellFanController/DellFanController/ReadCPUTemp.cs
@@ -0,0 +1,53 @@
+﻿// ====================================================================================================================
+// Application:      Dell R720 iDrac Fan Controller
+// Purpose:          Get CPU Details
+// Author:           Justin Bland
+// Date:             29/01/2022
+// Audit Date:       
+// Status:           In Development
+// Revisions
+//
+// References:       https://github.com/openhardwaremonitor/openhardwaremonitor/
+// ====================================================================================================================
+
+
+using OpenHardwareMonitor.Hardware;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DellFanController
+{
+    internal class ReadCPUTemp
+    {
+        internal float CPUTemp(float tCase)
+        {
+            float temp = ((float)(tCase * 0.75));  // Change this to 90% of TCase (Default max temp)
+
+            List<float> packageTemps = new List<float>();
+
+            var myComputer = new Computer
+            {
+                CPUEnabled = true
+            };
+            myComputer.Open();
+
+            foreach (var hardwareItem in myComputer.Hardware)
+            {
+                foreach (var sensor in hardwareItem.Sensors)
+                {
+                    if (sensor.Name == "CPU Package" && sensor.SensorType == SensorType.Temperature)
+                    {
+                        packageTemps.Add((float)sensor.Value);
+                    }
+                }
+            }
+
+            if (packageTemps.Count > 0)
+            {
+                temp = packageTemps.Max();
+            }
+
+            return temp;
+        }
+    }
+}
